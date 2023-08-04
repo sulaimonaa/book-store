3bec4966d40229e09b7ca47cf46883c741fba128
Sulaimon Azeez Ayodeji <sulaimonaa@gmail.com>
2023-08-04 22:54:13 +0100

    add styling to the navigation, UI, add span separator or divider to the display books

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import styles from '../styles.module.css';
+import user from '../assets/user.png';
 
 const Navigation = () => (
   <div className={`${styles.container_fluid} ${styles.header}`}>
@@ -7,8 +8,8 @@ const Navigation = () => (
       <h1 className={styles.primary_color}>Bookstore CMS</h1>
       <ul>
         <li>
-          <NavLink className={styles.link} to="/">
-            Home
+          <NavLink className={`${styles.link} ${styles.active}`} to="/">
+            Books
           </NavLink>
         </li>
         <li>
@@ -17,6 +18,9 @@ const Navigation = () => (
           </NavLink>
         </li>
       </ul>
+      <div className={styles.user}>
+        <img src={user} alt="user icon" className={styles.user_icon} />
+      </div>
     </nav>
   </div>
 );
